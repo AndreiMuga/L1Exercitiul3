4a3c5edfa85a9bc5ad6625640801d56218f097b2
Andrei <Andrei@DESKTOP-K91SUSC>
2024-03-19 21:52:01 +0200

    Add project files.

---
+++ Tema1Exercitiul3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace Lab1Ex3
+/*Ex3
+Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+tastaura
+ */
+{
+    public class Program 
+    { public static void Main(string[] args) 
+        { 
+         int a;
+         int ultimaCifra;
+         Console.WriteLine("Introduceti un numar"); 
+         a = Convert.ToInt32(Console.ReadLine()); 
+         ultimaCifra = a % 10; 
+        Console.WriteLine("Ultima cifra a numarului introdus este: " + ultimaCifra);
+        } 
+    } 
+}
